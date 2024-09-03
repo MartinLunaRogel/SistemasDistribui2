469faa8f7130502d3fd6929820441d92b8a6371b
MartinLunaRogel <mluna50@alumnos.uaq.mx>
2024-09-03 15:12:42 -0600

    Feat: Se suben archivos de SoapApi con las funciones GetAll, GetAllByEmail y GetAllById

---
+++ SoapApi/Repositories/IUserRepository.cs
@@ -0,0 +1,9 @@
+using SoapApi.Models;
+namespace SoapApi.Repositories;
+
+public interface IUserRepository{
+    Task<IList<UserModel>> GetAll(CancellationToken cancellationToken);
+    Task<IList<UserModel>> GetAllByEmail(string email, CancellationToken cancellationToken);
+    public Task<UserModel> GetByIdAsync(Guid id, CancellationToken cancellationToken) ;
+
+}
