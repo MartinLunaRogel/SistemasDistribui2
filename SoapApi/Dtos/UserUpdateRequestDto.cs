4f7796a3e3b786bd06d687da9a2bb660b8a374ee
MartinLunaRogel <mluna50@alumnos.uaq.mx>
2024-09-05 18:20:41 -0600

    Feat: Examen Primer Parcial

--- SoapApi/Dtos/UserUpdateRequestDto.cs
+++ SoapApi/Dtos/UserUpdateRequestDto.cs
@@ -1,4 +1,3 @@
-using System.Data;
 using System.Runtime.Serialization;
 
 namespace SoapApi.Dtos;
--- SoapApi/Program.cs
+++ SoapApi/Program.cs
@@ -1,19 +1,24 @@
-using Microsoft.Extensions.Options;
 using Microsoft.EntityFrameworkCore;
-using SoapApi.Infrastructure;
 using SoapCore;
+using SoapApi.Infrastructure;
 using SoapApi.Repositories;
 using SoapApi.Contracts;
 using SoapApi.Services;
 
 var builder = WebApplication.CreateBuilder(args);
+
 builder.Services.AddSoapCore();
 builder.Services.AddScoped<IUserRepository, UserRepository>();
 builder.Services.AddScoped<IUserContract, UserService>();
+builder.Services.AddScoped<IBookRepository, BookRepository>(); 
+builder.Services.AddScoped<IBookContract, BookService>();
 
-builder.Services.AddDbContext<RelationalDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddDbContext<RelationalDbContext>(options => 
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
+
 app.UseSoapEndpoint<IUserContract>("/UserService.svc", new SoapEncoderOptions());
+app.UseSoapEndpoint<IBookContract>("/BookService.svc", new SoapEncoderOptions());
 
 app.Run();
