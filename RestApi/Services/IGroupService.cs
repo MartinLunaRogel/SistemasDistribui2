3875cf24ea501e7ccbf58721b49b18e4426396a7
Martin Luna Rogel <134561698+MartinLunaRogel@users.noreply.github.com>
2024-09-24 13:14:40 -0600

    RestApi Update (#12)
    * Feat: Se suben archivos de DownloadFile
    
    * update: modify Dockerfile for automatic file creation
    
    * Feat: Se suben archivos de SoapApi con las funciones GetAll, GetAllByEmail y GetAllById
    
    * Feat: Se suben archivos de SoapApi con la implementacion de UpdateUser
    
    * Feat: Examen Primer Parcial
    
    * Feat: Se suben archivos de clase de RestApi
    
    * feat: add get groups by name

--- RestApi/Services/IGroupService.cs
+++ RestApi/Services/IGroupService.cs
@@ -3,6 +3,6 @@ using RestApi.Models;
 namespace RestApi.Services;
 
 public interface IGroupService{
-
     Task <GroupUserModel> GetGroupByIdAsync(string id, CancellationToken cancellationToken);
+    Task<IList<GroupUserModel>> GetGroupsByNameAsync(string name, CancellationToken cancellationToken);
 }
